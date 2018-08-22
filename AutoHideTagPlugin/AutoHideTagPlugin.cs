04d03909e79353a413c5c38f6dc1906bb04d1c90
lunax <w.swiatek12@gmail.com>
2018-08-22 13:28:02 +0200

    Push project

---
+++ AutoHideTagPlugin/AutoHideTagPlugin.cs
@@ -0,0 +1,42 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace AutoHideTagPlugin
+{
+    [PluginDetails(
+        author = "lunax",
+        name = "AutoHideTag",
+        description = "Automatically Hiding Tag for your Role",
+        id = "lunax.autohidetag",
+        version = "0.1",
+        SmodMajor = 3,
+        SmodMinor = 1,
+        SmodRevision = 12
+    )]
+    
+    class AutoHideTagPlugin : Plugin
+    {
+        
+        public static AutoHideTagPlugin plugin;
+        
+        public override void Register()
+        {
+            
+            this.AddConfig(new Smod2.Config.ConfigSetting("autohidetag_roles", new string[] {string.Empty}, Smod2.Config.SettingType.LIST, true, "Roles to auto hide tags"));
+            this.AddEventHandler(typeof(IEventHandlerPlayerJoin), new RoundEventHandler(this), Priority.Normal);
+        }
+
+        public override void OnEnable()
+        {
+            plugin = this;
+            this.Info(this.Details.name + " v." + this.Details.version + " - Enabled");    
+        }
+
+        public override void OnDisable()
+        {
+            this.Info(this.Details.name + " v." + this.Details.version + " - Disabled");
+        }
+    }
+}
---
+++ AutoHideTagPlugin/RoundEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+namespace AutoHideTagPlugin
+{
+    class RoundEventHandler :  IEventHandlerPlayerJoin
+    {
+        private Plugin plugin;
+        
+        public RoundEventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            if (this.plugin.GetConfigList("autohidetag_roles").Contains(ev.Player.GetRankName().ToLower()))
+                ev.Player.HideTag(true);
+        }
+    }
+}
